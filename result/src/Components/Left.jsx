c662ba98a31f1d26994707cb81b97e9e509a24ed
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-14 09:26:50 +0530

    components

---
+++ result/src/Components/Left.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Left() {
+  return (
+    <div>
+        Your Result
+  76
+  of 100
+
+  Great
+  You scored higher than 65% of the people who have taken these tests.
+      
+    </div>
+  )
+}
---
+++ result/src/Components/Right.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Right() {
+  return (
+    <div>
+      Summary
+
+Reaction
+80 / 100
+
+Memory
+92 / 100
+
+Verbal
+61 / 100
+
+Visual
+72 / 100
+
+Continue
+    </div>
+  )
+}
